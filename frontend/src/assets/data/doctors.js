78c928ed49e01540bfba6f0d3560d759797246d7
santina-appalo <santinaappalo@gmail.com>
2024-10-18 05:18:00 +0530

    update 1

--- frontend/src/assets/data/doctors.js
+++ frontend/src/assets/data/doctors.js
@@ -1,6 +1,6 @@
 import doctorImg01 from "../images/doctor-img01.png";
-import doctorImg02 from "..images/doctor-img02.png";
-import doctorImg03 from "..images/doctor-img03.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
 
 export const doctors = [
   {
