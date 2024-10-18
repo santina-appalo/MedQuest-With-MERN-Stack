bab7df31defb94615a5b8612489f089d6ecce179
santina-appalo <santinaappalo@gmail.com>
2024-10-18 13:13:13 +0530

    Update useTitle.js

--- src/Hooks/useTitle.js
+++ src/Hooks/useTitle.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const useTitle = (title) => {
   useEffect(() => {
-    document.title = `${title} - Medicare`;
+    document.title = `${title} - MedQuest`;
   }, [title]);
   return null;
 };
